9125c8fb47e7e9591f86bb3b5b4922f431294bb2
Waldirio M Pinheiro <waldirio@gmail.com>
2021-08-07 10:12:19 -0700

    new organization

---
+++ README.md
@@ -0,0 +1,35 @@
+# OCM-CLI Analyze
+
+Please, keep in mind you need your ocm-cli working as expected. After that, feel free to download this script and execute.
+
+```
+$ wget https://raw.githubusercontent.com/waldirio/c.rh.c-tools/master/ocm-cli_analyze/ocm-cli_analyze.sh
+$ chmod +x ocm-cli_analyze.sh
+$ ./ocm-cli_analyze.sh 
+Please, inform the cluster id
+exiting ...
+```
+As you can see above, it's necessary to pass the `cluster id`.
+
+```
+$ ./ocm-cli_analyze.sh 4e3d89be-567a-4dff-984e-5db63bbbXXXX
+Cluster Name or Label ..: 4e3d89be-567a-4dff-984e-5db63bbbXXXX
+Cluster ID .............: 1lfa4gc938civ1XXXXXXXXXXXXXXXXXXXXXX
+C.RH.C Link ............: https://console.redhat.com/openshift/details/s/1uGxSqOfREACi7mTujGOR5Uvb9A
+
+# Node Information
+{
+  "total": 6,
+  "master": 4,
+  "compute": 2
+}
+...
+```
+
+Just download the code and execute it in your local system.
+
+I really hope this helps you.
+
+If you need anything else of if you are facing issues trying to use it, please let me know.
+
+waldirio@redhat.com / waldirio@gmail.com
---
+++ ocm-cli_analyze.sh
@@ -0,0 +1,192 @@
+#!/bin/bash
+
+
+#
+# Developer .: Waldirio M Pinheiro <waldirio@redhat.com> | <waldirio@gmail.com>
+# Date ......: 08/01/2021 
+# Purpose ...: Use ocm-cli binary to check some OCP/OCM information
+#
+
+
+if [ "$1" == "" ]; then
+  echo "Please, inform the cluster id"
+  echo "exiting ..."
+  exit
+fi
+
+CLUSTER_ID_LABEL=$1
+CLUSTER_CHECK=$(ocm get /api/clusters_mgmt/v1/clusters --parameter search="display_name = '$CLUSTER_ID_LABEL'" | jq -r .items[].id | wc -l)
+CLUSTER_ID=$(ocm get /api/clusters_mgmt/v1/clusters --parameter search="display_name = '$CLUSTER_ID_LABEL'" | jq -r .items[].id)
+CLUSTER_WEB_ID=$(ocm get /api/clusters_mgmt/v1/clusters --parameter search="display_name = '$CLUSTER_ID_LABEL'" | jq -r .items[].subscription.id)
+
+
+if [ $CLUSTER_CHECK -eq 0 ]; then
+  echo "Invalid cluster"
+  echo "exiting ..."
+  exit
+fi
+
+CLUSTER_MGMT=$(ocm get /api/clusters_mgmt/v1/clusters --parameter search="display_name = '$CLUSTER_ID_LABEL'" | jq -r .items[])
+ACCOUNT_MGMT=$(ocm get /api/accounts_mgmt/v1/subscriptions --parameter search="cluster_id = '$CLUSTER_ID'" | jq -r .items[])
+
+# for debug purposes
+#echo "$CLUSTER_MGMT"
+#echo
+#echo "$ACCOUNT_MGMT"
+
+echo "Cluster Name or Label ..: $CLUSTER_ID_LABEL"
+echo "Cluster ID .............: $CLUSTER_ID"
+echo "C.RH.C Link ............: https://console.redhat.com/openshift/details/s/$CLUSTER_WEB_ID"
+echo
+
+cluster_node_info()
+{
+  echo "# Node Information"
+  echo $ACCOUNT_MGMT | jq .metrics[].nodes
+  echo
+}
+
+cluster_creation_date()
+{
+  echo "# Cluster Creation Date"
+  echo $CLUSTER_MGMT | jq -r .creation_timestamp
+  echo
+}
+
+cluster_version()
+{
+  echo "# Cluster Version"
+  echo $ACCOUNT_MGMT | jq -r .metrics[].openshift_version
+  echo
+}
+
+cluster_state()
+{
+  echo "# Cluster State"
+  echo $ACCOUNT_MGMT | jq -r .metrics[].state
+  echo $ACCOUNT_MGMT | jq -r .metrics[].state_description
+  echo
+}
+
+cluster_status()
+{
+  echo "# Cluster Status"
+  echo $ACCOUNT_MGMT | jq -r .status
+  echo
+}
+
+cluster_subscription()
+{
+  echo "# Cluster Subscription"
+  echo "Service level agreement (SLA) ...:" $(echo $ACCOUNT_MGMT | jq -r .support_level)
+  echo "Support type  ...................:" $(echo $ACCOUNT_MGMT | jq -r .service_level)
+  echo "Cluster usage  ..................:" $(echo $ACCOUNT_MGMT | jq -r .usage)
+  echo "Subscription units ..............:" $(echo $ACCOUNT_MGMT | jq -r .system_units)
+  echo "Subscriptions Obligations .......:" $(echo $ACCOUNT_MGMT | jq -r .metrics[].subscription_obligation_exists)
+  echo
+}
+
+cluster_etcd_encryption()
+{
+  echo "# Cluster ETCD Encryption"
+  echo $CLUSTER_MGMT | jq -r .etcd_encryption
+  echo
+}
+
+cluster_billing_model()
+{
+  echo "# Cluster Billing Model"
+  echo $ACCOUNT_MGMT | jq -r .cluster_billing_model
+  echo
+}
+
+managed_cluster()
+{
+  echo "# Managed Cluster"
+  echo $ACCOUNT_MGMT | jq -r .managed
+  echo
+}
+
+provenance()
+{
+  echo "# Provenance"
+  echo $ACCOUNT_MGMT | jq -r .provenance
+  echo
+}
+
+reconcile()
+{
+  echo "# Last Reconcile Date"
+  echo $ACCOUNT_MGMT | jq -r .last_reconcile_date
+  echo
+}
+
+console_url()
+{
+  echo "# Console URL"
+  echo $ACCOUNT_MGMT | jq -r .console_url
+  echo
+}
+
+health_state()
+{
+  echo "# Health State"
+  echo $ACCOUNT_MGMT | jq -r .metrics[].health_state
+  echo
+}
+
+
+cluster_memory()
+{
+  echo "# Cluster Memory Summary"
+  echo $ACCOUNT_MGMT | jq -r .metrics[].memory
+  echo
+  ocm cluster status $CLUSTER_ID
+  echo
+}
+
+
+cluster_upgrade()
+{
+  echo "# Cluster Upgrade"
+  echo $ACCOUNT_MGMT | jq -r .metrics[].upgrade
+  echo
+}
+
+cluster_cloud_provider()
+{
+  echo "# Cloud Provider"
+  echo $ACCOUNT_MGMT | jq -r .metrics[].cloud_provider
+  echo $ACCOUNT_MGMT | jq -r .metrics[].region
+  echo
+}
+
+cluster_alerts()
+{
+  echo "# Cluster Alerts"
+  echo "Critical Alerts Firing .......:" $(echo $ACCOUNT_MGMT | jq -r .metrics[].critical_alerts_firing)
+  echo "Operators Condition Failing ..:" $(echo $ACCOUNT_MGMT | jq -r .metrics[].operators_condition_failing)
+  echo
+}
+
+
+
+## Main
+
+cluster_node_info
+cluster_subscription
+managed_cluster
+cluster_status
+provenance
+reconcile
+console_url
+health_state
+cluster_memory
+cluster_upgrade
+cluster_state
+cluster_version
+cluster_cloud_provider
+cluster_alerts
+cluster_billing_model
+cluster_etcd_encryption
+cluster_creation_date
